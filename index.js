2f3f60c40f108787b4fe508d6e95dbe534c48363
sachin <sksachinkaushik5362@gmail.com>
2023-05-25 19:59:26 +0530

    accept request and send the response

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express=require('express');
 const app=express();
 
 app.get('',(req,resp)=>{
-    resp.send('Hello ,This is Home  page');
+    let getData=req.query.name;
+    resp.send('Welcome '+getData+' ,  This is Home  page and Welcome  ');
 });
 
 app.get('/about',(req,resp)=>{
-    resp.send('Hello ,This is About  page');
+    
+    resp.send('Welcome  ,  This is About  page and Welcome  ');
 });
 app.get('/contact',(req,resp)=>{
-    resp.send('Hello ,This is contact  page');
+    
+    resp.send('Welcome ,  This is Contact  page and Welcome  ');
 });
 
 app.listen(5000);
