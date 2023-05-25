1e9ba10a6f8c988e7a8f1a5ef6f0cc4cec677f2e
sachin <sksachinkaushik5362@gmail.com>
2023-05-25 20:58:05 +0530

    removing extension from html page

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const path=require('path');
 const app=express();
 
 const publicPath=path.join(__dirname,'public');
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+app.get('',(req,resp)=>{
+    resp.sendFile(`
+    ${publicPath}/index.html
+    `)
+})
+app.get('/about',(req,resp)=>{
+    resp.sendFile(`
+    ${publicPath}/about.html
+    `)
+})
+app.get('/contact',(req,resp)=>{
+    resp.sendFile(` ${publicPath}/contact.html `)
+})
 
 
 
