71611e7119c1eeee888150837de80d60b7a3200a
sachin <sksachinkaushik5362@gmail.com>
2023-05-25 15:09:41 +0530

    Creating File using File system

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-console.log(process.argv)
-console.log(process.argv[2])
-console.log(process.argv[3])
-console.log(process.argv[4])
+const fs=require('fs');
+const input=process.argv;
+
+if(input [2]=='add'){
+    fs.writeFileSync(input[3],input[4]);
+
+}
+
+else if(input[2]=='remove'){
+    fs.unlinkSync(input[3]);
+}
+
+else {
+    console.log("Pls Enter Valid Input")
+}
