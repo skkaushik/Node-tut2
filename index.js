5a2ee163169c576a3a9a95270e2c0f5feee02144
sachin <sksachinkaushik5362@gmail.com>
2023-05-26 13:14:05 +0530

    importing middleware

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express=require('express');
 const app= express();
+const reqFilter=require('./middleware');
 
-const reqFilter=(req,resp,next)=>{
-    if(!req.query.age){
-        resp.send("Please Provide age")
-    }
-    else if(req.query.age<18){
-        resp.send("You can not access this page")
-    }
-    else
-    {
-        next();
-    }
-}
 
-app.use(reqFilter);
+
+// app.use(reqFilter);
 
 app.get('/', (req,resp)=>{
 resp.send('Welcome to Home page')
 });
 
-app.get('/user', (req,resp)=>{
+app.get('/user',reqFilter, (req,resp)=>{
     resp.send('Welcome to User page')
     });
+app.get('/Payment',reqFilter, (req,resp)=>{
+        resp.send('Welcome to Payement page')
+        });
+app.get('/about', (req,resp)=>{
+        resp.send('Welcome to About Us page')
+        });
+
+app.get('/contact', (req,resp)=>{
+        resp.send('Welcome to Contact page')
+        });
+    
+
 
 app.listen(4300)
