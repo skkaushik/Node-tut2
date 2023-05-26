36620774dc2a4fa5ce1319b4a1fd6ffdaf61f150
sachin <sksachinkaushik5362@gmail.com>
2023-05-26 22:59:17 +0530

    connecting and reading data from mongodb

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-const express=require('express');
-const app= express();
-const reqFilter=require('./middleware');
+const getData=require('./mongodb')
+// getData().then((response)=>{
+//         response.find({}).toArray().then((data)=>{
+//                 console.log(data)
+//         })
 
+// });
 
+const main=async ()=>{
+        let data = await getData();
+        data= await data.find().toArray();
+        console.log(data);
+}
 
-// app.use(reqFilter);
-
-app.get('/', (req,resp)=>{
-resp.send('Welcome to Home page')
-});
-
-app.get('/user',reqFilter, (req,resp)=>{
-    resp.send('Welcome to User page')
-    });
-app.get('/Payment',reqFilter, (req,resp)=>{
-        resp.send('Welcome to Payement page')
-        });
-app.get('/about', (req,resp)=>{
-        resp.send('Welcome to About Us page')
-        });
-
-app.get('/contact', (req,resp)=>{
-        resp.send('Welcome to Contact page')
-        });
-    
-
-
-app.listen(4300)
+main();
---
+++ middleware.js
@@ -0,0 +1,12 @@
+module.exports=reqFilter=(req,resp,next)=>{
+    if(!req.query.age){
+        resp.send("Please Provide age")
+    }
+    else if(req.query.age<18){
+        resp.send("You can not access this page")
+    }
+    else
+    {
+        next();
+    }
+}
