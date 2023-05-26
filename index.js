4cc86ef91acdfa83af20882c0f4b67de2a0806ff
sachin <sksachinkaushik5362@gmail.com>
2023-05-26 10:11:57 +0530

    creating dynamic webpage  ejs templating

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const path=require('path');
 const app=express();
 
 const publicPath=path.join(__dirname,'public');
-// app.use(express.static(publicPath));
-// console.log(__dirname+`/public/index.html`);
+
+app.set('view engine','ejs');
+
 app.get('',(req,resp)=>{
     resp.sendFile(`${publicPath}/index.html`);
-})
+});
 app.get('/about',(req,resp)=>{
     resp.sendFile(`${publicPath}/about.html`)
-})
+});
 app.get('/contact',(req,resp)=>{
     resp.sendFile(`${publicPath}/contact.html`)
+});
+app.get('/profile',(req,resp)=>{
+    const user={
+        name:'Sachin Kaushik',
+        Email:'skkaushik63728gmail.com',
+        Address:'Agra'
+    }
+    resp.render('profile',{user});
 })
 
 
