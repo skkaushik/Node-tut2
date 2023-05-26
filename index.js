e1b437901c39a003790fdc2460517308dabe8b98
sachin <sksachinkaushik5362@gmail.com>
2023-05-27 02:43:10 +0530

    Crud with mongoose

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const mongoose=require('mongoose');
-const main=async()=>{
-        await mongoose.connect("mongodb://127.0.0.1:27017/RECB");
+ mongoose.connect("mongodb://127.0.0.1:27017/RECB");
         const StudentsSchema= new mongoose.Schema(
                 {
                         name:String,
-                        marks:Number
+                        marks:Number,
+                        email1:String,
+                        address:String
+
                 });
+const saveDatainDb=async()=>{
+        
 
         const StudentsModel =new mongoose.model('Students',StudentsSchema)
-        let data =new StudentsModel({name:'Rachit',marks:76});
+        let data =new StudentsModel({name:'Anju',marks:70,email1:'beautiqueen@gmail.com',address:'Balia'});
         let result = await data.save();
         console.log(result)
 
 }
 
-main();
+// saveDatainDb();
+const updateData=async()=>{
+        const Student=mongoose.model('Students',StudentsSchema);
+        let data = await Student.updateOne(
+                {
+                        name:'Rachit'
+                },
+                {$set:{email1:'rachit@gamil.com'}}
+        )
+        console.log(data)
+}
+
+// updateData();
+const deleteInDB = async ()=>{
+        const Student = mongoose.model('Students', StudentsSchema);
+        let data = await Student.deleteOne({name:'Sachin'})
+        console.log(data);
+    }
+
+// deleteInDB();
+    const findInDB = async ()=>{
+        const Student = mongoose.model('Students', StudentsSchema);
+        let data = await Student.find({name:'Anju'})
+        console.log(data);
+    }
+    
+    findInDB();
