9fce21ad5e17d307c46d2aff49753115909ecbee
sachin <sksachinkaushik5362@gmail.com>
2023-05-27 00:37:31 +0530

    inserting data to database collection

---
+++ insert.js
@@ -0,0 +1,13 @@
+const getData=require('./mongodb')
+
+
+const insert=async ()=>{
+        const db = await getData();
+       const result= await db.insert(
+            {name:'Sahil',branch:"IT",address:'Raya',phoneno:6547328720
+
+        });
+        // console.log(result);
+}
+
+insert();
--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 const { response } = require("express");
 const {MongoClient}= require("mongodb");
-const url ='mongodb://localhost:27017';
+const url ='mongodb://127.0.0.1:27017';
 const database='RECB';
 const client =new MongoClient(url);
 
@@ -8,8 +8,7 @@ async function getData(){
         let result=await client.connect();
         let db=result.db(database);
         return db.collection('Students');
-        // let response=await collection.find({}).toArray();
-        // console.log(response);
+        
 
 }
 
