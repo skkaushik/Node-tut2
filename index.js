70e5029a89b61080278b28331934b0acfd56ef0c
sachin <sksachinkaushik5362@gmail.com>
2023-05-27 09:56:34 +0530

    search api

--- index.js
+++ index.js
@@ -4,40 +4,21 @@ const Student=require('./Students');
 
 const app = exppress();
 app.use(exppress.json());
-app.post("/create",async(req,resp)=>{
-        let data=new Student(req.body)
-        let result= await data.save()
-        console.log(result)
-        resp.send(result);
-});
 
-app.get("/list",async (req,resp)=>{
-        let data=await Student.find();
-        resp.send(data);
-
-
-});
-
-app.delete("/delete/:_id",async (req,resp)=>{
-        console.log(req.params)
-        let data = await Student.deleteOne(req.params)
-        resp.send(data);
-
-
-});
-
-app.put("/update/:_id",async (req,resp)=>{
-        console.log(req.params)
-        let data = await Student.updateOne(
-                req.params,
+app.get("/search/:key",async (req,resp)=>{
+        console.log(req.params.key)
+        let data=await Student.find(
                 {
-                  $set:req.body      
+
+                        "$or":[
+                                {"name":{$regex:req.params.key}}
+                        ]
                 }
-                )
+        );
         resp.send(data);
 
+})
 
-});
 
 
 
